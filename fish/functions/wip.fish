3def4e1484ef169767a39e5eecbeb88120446779
Joakim Bergros <joakim.bergros@svedala.se>
2025-09-03 19:19:04 +0200

    restructured wip aliases

--- fish/functions/wip.fish
+++ fish/functions/wip.fish
@@ -1,4 +1,3 @@
 function wip --wraps='git add . && git commit -m "WIP"' --wraps='git add . && git commit -m "wip"' --description 'alias wip git add . && git commit -m "wip"'
-  git add . && git commit -m "wip" $argv
-        
+    git add . && git commit -m $argv
 end
--- fish/functions/wipp.fish
+++ fish/functions/wipp.fish
@@ -1,4 +1,3 @@
 function wipp --wraps='git add . && git commit -m "wip" && git push' --description 'alias wipp git add . && git commit -m "wip" && git push'
-  git add . && git commit -m "wip" && git push $argv
-        
+    wip $argv && git push
 end
